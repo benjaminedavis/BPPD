6a88acc9daa0ef7c3cdc3983a83c3de6f3824c21
Philippe Luchansky <philuchansky@gmail.com>
2015-08-19 11:50:19 -0700

    remove test login route, and use app router to /signin

--- routes/users.js
+++ routes/users.js
@@ -12,9 +12,4 @@ router.get('/delete', function(req, res, next) {
   // res.send('confirm delete page')
 });
 
-router.post('/testlogin', function(req,res) {
-	console.log(req.body);
-	res.redirect('/');
-});
-
 module.exports = router;
