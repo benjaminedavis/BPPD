4c297a60515653a03cbc81efa1e40f82950a63dd
Ben Davis <benjaminedavis@gmail.com>
2015-08-19 08:00:12 -0700

    added new post get put for user page

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,32 @@ router.get('/', function(req, res, next) {
 /* DELETE individual user. */
 router.get('/delete', function(req, res, next) {
   res.render('../views/delete');
-  // res.send('confirm delete page')
 });
+
+
+// bens new stuff
+
+// CREATE a new user. just created */
+// we need a new.ejs
+router.post('/new', function(req, res, next) {
+  res.send('successful /new test')
+  // res.render('../views/new');
+});
+
+// GET a user's page. this is where saved music is kept
+// we need a show.ejs
+router.get('/:id', function(req, res, next) {
+  res.send('successful /:id test')
+  // res.render('../views/show');
+});
+
+// EDIT a user's page. to add/delete music
+router.put('/:id/edit', function(req, res, next) {
+  res.send('successful :id/edit test')
+  // res.render('../views/show');
+})
+
+// bens new stuff
+
 
 module.exports = router;
