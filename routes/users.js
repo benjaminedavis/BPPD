c0d1e7165eea22a9fa7be0542f015b28999f9170
Philippe Luchansky <philuchansky@gmail.com>
2015-08-20 14:42:49 -0700

    add form action, shorten render string

--- routes/users.js
+++ routes/users.js
@@ -51,7 +51,7 @@ router.get('/delete', function(req, res, next) {
 });
 /* NEW users listing. */
 router.get('/new', function(req, res, next) {
-  res.render('new.ejs', {title: 'Create New Account', token: req.token, });
+  res.render('new', {title: 'Create New Account', token: req.token, });
 });
 
 
